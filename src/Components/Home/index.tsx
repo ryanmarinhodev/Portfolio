661fbeaf5fff0cfaaecbc101089a1f0c52c97abb
ryanmarinhodev <ryanmarinhodev@gmail.com>
2026-05-14 08:33:53 -0300

    feat: my-curriculum

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           </p>
 
           <div className="secao-link">
-            <a className="link link-baixarCV" href="public/Curriculo-Ryan.pdf" download>
+            <a className="link link-baixarCV" href="public/curriculo-developer-ryanmarinho.pdf" download>
               Baixar CV
             </a>
             <a className="link link-meus-projetos" href="#projetos">
